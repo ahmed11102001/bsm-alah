532204344d8ce7a228845e7522f951daf14efaaf
ahmed11102001 <a1110hmedadel@gmail.com>
2026-04-14 23:40:43 +0200

    بسم الله

--- src/components/dashboard/Contacts.tsx
+++ src/components/dashboard/Contacts.tsx
@@ -8,7 +8,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Plus, Trash2 } from "lucide-react";
 
-export default function AudiencesPage() {
+export default function Contacts() {
   const [audiences, setAudiences] = useState<any[]>([]);
   const [isOpen, setIsOpen] = useState(false);
   const [step, setStep] = useState(1);
