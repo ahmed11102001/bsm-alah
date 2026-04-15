740d2bfac0ee9b744b25adf7fd980d32d788d36d
ahmed11102001 <a1110hmedadel@gmail.com>
2026-04-15 21:47:03 +0200

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 
 import { signOut, useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
