ba0736d366161908836ca71998eaef1750c2e845
ahmed11102001 <a1110hmedadel@gmail.com>
2026-05-16 19:51:23 +0300

    df

--- Vitest.config .ts
+++ Vitest.config .ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { fileURLToPath } from "url";
 import path from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     environment: "node",
     globals: true,
