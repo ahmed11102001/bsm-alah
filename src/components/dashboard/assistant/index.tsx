f6545a5c5cac87ee4b75d6a63bd987f7f04aa956
ahmed11102001 <a1110hmedadel@gmail.com>
2026-05-23 21:43:14 +0300

    Update index.tsx

--- src/components/dashboard/assistant/index.tsx
+++ src/components/dashboard/assistant/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect, useCallback } from "react";
-import WelcomeBanner  from "./welcomebanner";
+import WelcomeBanner from "./WelcomeBanner";
 import RuleBanner     from "./RuleBanner";
 import FloatingHelper from "./FloatingHelper";
 import {
