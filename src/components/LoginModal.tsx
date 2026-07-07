cf987873da3ce137da42aa766901bae7622ccaa4
ahmed11102001 <a1110hmedadel@gmail.com>
2026-07-07 17:31:26 +0300

    Update LoginModal.tsx

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -548,6 +548,7 @@ export default function LoginModal({ isOpen, onClose, callbackUrl }: LoginModalP
             )}
 
           </AnimatePresence>
+          </div>
         </div>
       </DialogContent>
     </Dialog>
